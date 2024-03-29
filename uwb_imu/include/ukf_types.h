8259e024d157a910b5891d121b55f2cfcf45cc58
Yonghee Kim <121211187+KYH04444@users.noreply.github.com>
2024-03-29 12:52:19 +0900

    Update ukf_types.h

--- uwb_imu/include/ukf_types.h
+++ uwb_imu/include/ukf_types.h
@@ -2,14 +2,11 @@
 #define _UKF_TYPES_H_
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include <cmath>
+namespace UKF {
 
-namespace UKF
-{
-
-// imu data struct
 template <typename scalar>
-struct ImuData
-{
+struct ImuData {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     double stamp;
     Eigen::Matrix<scalar, 3, 1> acc;
@@ -17,55 +14,48 @@ struct ImuData
 };
 
 template <typename scalar>
-
-struct UwbData
-{
+struct UwbData {
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
     Eigen::Matrix<scalar, 3, 1> data;
     Eigen::Matrix<scalar, 3, 3> cov;
 };
 
-// formula: 156
-template <typename scalar>
 struct STATE {
     Eigen::Matrix3d Rot;
     Eigen::Vector3d v, p, b_gyro, b_acc;
 };
 
-template <typename scalar>
-struct WEIGHTS 
-{
-    W red_d;
-    W q;
-    W up_d;
-    W aug_d;
-    W aug_q;
+template <typename T>
+struct W {
+    T sqrt_d_lambda;
+    T wj;
+    T wm;
+    T w0;
 
-    WEIGHTS(double red_d_val, double q_val, double up_d_val, double aug_d_val, double aug_q_val, double alpha[])
-    : red_d(red_d_val, alpha[0]),
-      q(q_val, alpha[1]),
-      up_d(up_d_val, alpha[2]),
-      aug_d(aug_d_val, alpha[3]),
-      aug_q(aug_q_val, alpha[4]) 
-};
-
-template <typename scalar>
-struct W 
-{
-    double sqrt_d_lambda;
-    double wj;
-    double wm;
-    double w0;
-
-    W(double l, double alpha) {
-        double m = (std::pow(alpha, 2) - 1) * l;
+    W(T l, T alpha) {
+        T m = (std::pow(alpha, 2) - 1) * l;
         sqrt_d_lambda = std::sqrt(l + m);
         wj = 1 / (2 * (l + m));
-        wm = m / (m + l);
-        w0 = m / (m + l) + 3 - std::pow(alpha, 2);
+        wm = m / (l + m);
+        w0 = m / (l + m) + 3 - std::pow(alpha, 2);
     }
 };
 
+struct WEIGHTS {
+    W<double> red_d;
+    W<double> q;
+    W<double> up_d;
+    W<double> aug_d;
+    W<double> aug_q;
 
-} // namespace Fusion
+    WEIGHTS(double red_d_val, double q_val, double up_d_val, double aug_d_val, double aug_q_val, double alpha[]) :
+        red_d(red_d_val, alpha[0]),
+        q(q_val, alpha[1]),
+        up_d(up_d_val, alpha[2]),
+        aug_d(aug_d_val, alpha[3]),
+        aug_q(aug_q_val, alpha[4])
+    {}
+};
+
+} // namespace UKF
 #endif
