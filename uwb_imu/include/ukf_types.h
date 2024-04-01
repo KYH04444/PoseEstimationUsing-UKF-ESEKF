b2712db6ca3935e76e70f98ab148856a8fb52060
Yonghee Kim <121211187+KYH04444@users.noreply.github.com>
2024-04-01 22:47:54 +0900

    Update ukf_types.h

--- uwb_imu/include/ukf_types.h
+++ uwb_imu/include/ukf_types.h
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include <cmath>
+using namespace Eigen;
 namespace UKF {
 
 template <typename scalar>
@@ -24,38 +25,69 @@ struct STATE {
     Eigen::Matrix3d Rot;
     Eigen::Vector3d v, p, b_gyro, b_acc;
 };
-
-template <typename T>
-struct W {
-    T sqrt_d_lambda;
-    T wj;
-    T wm;
-    T w0;
-
-    W(T l, T alpha) {
-        T m = (std::pow(alpha, 2) - 1) * l;
-        sqrt_d_lambda = std::sqrt(l + m);
-        wj = 1 / (2 * (l + m));
-        wm = m / (l + m);
-        w0 = m / (l + m) + 3 - std::pow(alpha, 2);
-    }
-};
-
 struct WEIGHTS {
-    W<double> red_d;
-    W<double> q;
-    W<double> up_d;
-    W<double> aug_d;
-    W<double> aug_q;
 
-    WEIGHTS(double red_d_val, double q_val, double up_d_val, double aug_d_val, double aug_q_val, double alpha[]) :
-        red_d(red_d_val, alpha[0]),
-        q(q_val, alpha[1]),
-        up_d(up_d_val, alpha[2]),
-        aug_d(aug_d_val, alpha[3]),
-        aug_q(aug_q_val, alpha[4])
-    {}
+
+    // template <typename T>
+    struct W {
+        double sqrt_d_lambda;
+        double wj;
+        double wm;
+        double w0;
+
+        W(double l, double alpha) {
+            double m = (std::pow(alpha, 2) - 1) * l;
+            sqrt_d_lambda = std::sqrt(l + m);
+            wj = 1 / (2 * (l + m));
+            wm = m / (l + m);
+            w0 = m / (l + m) + 3 - std::pow(alpha, 2);
+        }
+    };
+    
+    W red_d;
+    W q;
+    W up_d;
+    W aug_d;
+    W aug_q;
+
+    WEIGHTS(double red_d_val, double q_val, double up_d_val, double aug_d_val, double aug_q_val, double* alpha) :
+    red_d(red_d_val, alpha[0]),
+    q(q_val, alpha[1]),
+    up_d(up_d_val, alpha[2]),
+    aug_d(aug_d_val, alpha[3]),
+    aug_q(aug_q_val, alpha[4]) {}
 };
+// template <typename T>
+// struct W {
+//     T sqrt_d_lambda;
+//     T wj;
+//     T wm;
+//     T w0;
+
+//     W(T l, T alpha) {
+//         T m = (std::pow(alpha, 2) - 1) * l;
+//         sqrt_d_lambda = std::sqrt(l + m);
+//         wj = 1 / (2 * (l + m));
+//         wm = m / (l + m);
+//         w0 = m / (l + m) + 3 - std::pow(alpha, 2);
+//     }
+// };
+
+// struct WEIGHTS {
+//     W<double> red_d;
+//     W<double> q;
+//     W<double> up_d;
+//     W<double> aug_d;
+//     W<double> aug_q;
+
+//     WEIGHTS(double red_d_val, double q_val, double up_d_val, double aug_d_val, double aug_q_val, double* alpha) :
+//         red_d(red_d_val, alpha[0]),
+//         q(q_val, alpha[1]),
+//         up_d(up_d_val, alpha[2]),
+//         aug_d(aug_d_val, alpha[3]),
+//         aug_q(aug_q_val, alpha[4])
+//     {}
+// };
 
 } // namespace UKF
 #endif
