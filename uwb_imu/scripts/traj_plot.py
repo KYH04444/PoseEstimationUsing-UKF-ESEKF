2bd49a916e4c24ffb423d9919b9fc7da9bea789a
Yonghee Kim <121211187+KYH04444@users.noreply.github.com>
2024-04-07 18:52:04 +0900

    Update traj_plot.py

--- uwb_imu/scripts/traj_plot.py
+++ uwb_imu/scripts/traj_plot.py
@@ -5,26 +5,42 @@ from mpl_toolkits.mplot3d import Axes3D
 from uwb_imu.msg import UwbMsg
 class Plotter:
     def __init__(self):
-        self.x_uwb = []
-        self.y_uwb = []
-        self.z_uwb = []
+        self.esekf_x_uwb = []
+        self.esekf_y_uwb = []
+        self.esekf_z_uwb = []
+
+        self.ukf_x_uwb = []
+        self.ukf_y_uwb = []
+        self.ukf_z_uwb = []
+
         self.x_local = []
         self.y_local = []
         self.z_local = []
-        self.uwb_cnt = 0
+        self.esekf_uwb_cnt = 0
+        self.ukf_uwb_cnt = 0
         self.imu_cnt = 0
         rospy.init_node("plotter")
-        rospy.Subscriber("/result", PoseStamped, self.uwb_callback)
+        rospy.Subscriber("/result", PoseStamped, self.esekf_uwb_callback)
+        rospy.Subscriber("/result_ukf", PoseStamped, self.ukf_uwb_callback)
         rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.local_callback)
 
-    def uwb_callback(self, msg):
-        self.uwb_cnt+=1
-        if self.uwb_cnt >= 3:
-            self.x_uwb.append(msg.pose.position.x)
-            self.y_uwb.append(msg.pose.position.y)
-            self.z_uwb.append(msg.pose.position.z)
+    def esekf_uwb_callback(self, msg):
+        self.esekf_uwb_cnt+=1
+        if self.esekf_uwb_cnt >= 3:
+            self.esekf_x_uwb.append(msg.pose.position.x)
+            self.esekf_y_uwb.append(msg.pose.position.y)
+            self.esekf_z_uwb.append(msg.pose.position.z)
             # print("ing")
-            self.uwb_cnt = 0
+            self.esekf_uwb_cnt = 0
+
+    def ukf_uwb_callback(self, msg):
+        self.ukf_uwb_cnt+=1
+        if self.ukf_uwb_cnt >= 3:
+            self.ukf_x_uwb.append(msg.pose.position.x)
+            self.ukf_y_uwb.append(msg.pose.position.y)
+            self.ukf_z_uwb.append(msg.pose.position.z)
+            # print("ing")
+            self.ukf_uwb_cnt = 0
 
     def local_callback(self, msg):
         self.imu_cnt+=1
@@ -33,6 +49,7 @@ class Plotter:
             self.y_local.append(msg.pose.position.y)
             self.z_local.append(msg.pose.position.z)
             self.imu_cnt = 0
+    
     def plot_data(self):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
@@ -40,7 +57,8 @@ class Plotter:
         while not rospy.is_shutdown():
             ax.clear()
 
-            ax.scatter(self.x_uwb, self.y_uwb, self.z_uwb, c='r', label='ESEKF', s=1)
+            ax.scatter(self.esekf_x_uwb, self.esekf_y_uwb, self.esekf_z_uwb, c='r', label='ESEKF', s=1)
+            ax.scatter(self.ukf_x_uwb, self.ukf_y_uwb, self.ukf_z_uwb, c='g', label='UKF', s=1)
             ax.scatter(self.x_local, self.y_local, self.z_local, c='b', label='GT',s=5)
 
             # ax.scatter(self.x_uwb, self.y_uwb, c='r', label='UWB Filtered', s=1)
