933a587813459e8ecefe904a7507eb47dc0481df
Yonghee Kim <121211187+KYH04444@users.noreply.github.com>
2024-04-10 17:09:17 +0900

    Update traj_plot.py

--- uwb_imu/scripts/traj_plot.py
+++ uwb_imu/scripts/traj_plot.py
@@ -22,7 +22,7 @@ class Plotter:
         rospy.init_node("plotter")
         rospy.Subscriber("/result", PoseStamped, self.esekf_uwb_callback)
         rospy.Subscriber("/result_ukf", PoseStamped, self.ukf_uwb_callback)
-        rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.local_callback)
+        rospy.Subscriber("/uwb_filtered", UwbMsg, self.local_callback)
 
     def esekf_uwb_callback(self, msg):
         self.esekf_uwb_cnt+=1
@@ -45,9 +45,9 @@ class Plotter:
     def local_callback(self, msg):
         self.imu_cnt+=1
         if self.imu_cnt >= 3:
-            self.x_local.append(msg.pose.position.x)
-            self.y_local.append(msg.pose.position.y)
-            self.z_local.append(msg.pose.position.z)
+            self.x_local.append(msg.pos_x)
+            self.y_local.append(msg.pos_y)
+            self.z_local.append(msg.pos_z)
             self.imu_cnt = 0
     
     def plot_data(self):
@@ -60,9 +60,6 @@ class Plotter:
             ax.scatter(self.esekf_x_uwb, self.esekf_y_uwb, self.esekf_z_uwb, c='r', label='ESEKF', s=1)
             ax.scatter(self.ukf_x_uwb, self.ukf_y_uwb, self.ukf_z_uwb, c='g', label='UKF', s=1)
             ax.scatter(self.x_local, self.y_local, self.z_local, c='b', label='GT',s=5)
-
-            # ax.scatter(self.x_uwb, self.y_uwb, c='r', label='UWB Filtered', s=1)
-            # ax.scatter(self.x_local, self.y_local,  c='b', label='Local Position',s=5)
 
             ax.set_xlabel('X')
             ax.set_ylabel('Y')
