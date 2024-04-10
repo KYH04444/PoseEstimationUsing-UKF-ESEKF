96a1ffec78d4c06350d28096bda80cf01fd665e1
Yonghee Kim <121211187+KYH04444@users.noreply.github.com>
2024-04-10 17:10:48 +0900

    Create uwb.py

---
+++ uwb_imu/scripts/uwb.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+"""
+The Pozyx ready to localize tutorial (c) Pozyx Labs
+Please read the tutorial that accompanies this sketch:
+https://www.pozyx.io/Documentation/Tutorials/ready_to_localize/Python
+
+This tutorial requires at least the contents of the Pozyx Ready to Localize kit. It demonstrates the positioning capabilities
+of the Pozyx device both locally and remotely. Follow the steps to correctly set up your environment in the link, change the
+parameters and upload this sketch. Watch the coordinates change as you move your device around!
+"""
+from time import sleep
+import rospy
+from uwb_imu.msg import UwbMsg
+from pypozyx import (POZYX_POS_ALG_UWB_ONLY, POZYX_3D, Coordinates, POZYX_SUCCESS, PozyxConstants, version,
+                     DeviceCoordinates, PozyxSerial, get_first_pozyx_serial_port, SingleRegister, DeviceList, PozyxRegisters)
+from pythonosc.udp_client import SimpleUDPClient
+
+from pypozyx.tools.version_check import perform_latest_version_check
+
+
+class ReadyToLocalize(object):
+    """Continuously calls the Pozyx positioning function and prints its position."""
+
+    def __init__(self, pozyx, osc_udp_client, anchors, algorithm=POZYX_POS_ALG_UWB_ONLY, dimension=POZYX_3D, height=1000, remote_id=None):
+        self.pozyx = pozyx
+        self.osc_udp_client = osc_udp_client    
+
+        self.anchors = anchors
+        self.algorithm = algorithm
+        self.dimension = dimension
+        self.height = height
+        self.remote_id = remote_id
+        self.uwb_msg = UwbMsg()
+
+    def setup(self):
+        """Sets up the Pozyx for positioning by calibrating its anchor list."""
+        print("------------POZYX POSITIONING V{} -------------".format(version))
+        print("")
+        print("- System will manually configure tag")
+        print("")
+        print("- System will auto start positioning")
+        print("")
+        if self.remote_id is None:
+            self.pozyx.printDeviceInfo(self.remote_id)
+        else:
+            for device_id in [None, self.remote_id]:
+                self.pozyx.printDeviceInfo(device_id)
+        print("")
+        print("------------POZYX POSITIONING V{} -------------".format(version))
+        print("")
+
+        self.setAnchorsManual(save_to_flash=False)
+        self.printPublishConfigurationResult()
+        # return(uwb_msg)
+    
+    def loop(self):
+        """Performs positioning and displays/exports the results."""
+        position = Coordinates()
+        status = self.pozyx.doPositioning(
+            position, self.dimension, self.height, self.algorithm, remote_id=self.remote_id)
+        if status == POZYX_SUCCESS:
+            self.printPublishPosition(position)
+        else:
+            self.printPublishErrorCode("positioning")
+        # return uwb_msg
+    
+    def printPublishPosition(self, position):
+        """Prints the Pozyx's position and possibly sends it as a OSC packet"""
+        network_id = self.remote_id
+        if network_id is None:
+            network_id = 0
+        print("POS ID {}, x(m): {pos.x} y(m): {pos.y} z(m): {pos.z}".format(
+            "0x%0.4x" % network_id, pos=position))
+        if self.osc_udp_client is not None:
+            self.osc_udp_client.send_message(
+                "/position", [network_id, int(position.x), int(position.y), int(position.z)])
+            # if position.x is not None:
+                # uwb_msg = UwbMsg()
+            self.uwb_msg.header.stamp = rospy.Time.now()
+            self.uwb_msg.pos_x = int(position.x)
+            self.uwb_msg.pos_y = int(position.y)
+            self.uwb_msg.pos_z = int(position.z)
+            # return(uwb_msg)
+
+    def printPublishErrorCode(self, operation):
+        """Prints the Pozyx's error and possibly sends it as a OSC packet"""
+        error_code = SingleRegister()
+        network_id = self.remote_id
+        if network_id is None:
+            self.pozyx.getErrorCode(error_code)
+            print("LOCAL ERROR %s, %s" % (operation, self.pozyx.getErrorMessage(error_code)))
+            if self.osc_udp_client is not None:
+                self.osc_udp_client.send_message("/error", [operation, 0, error_code[0]])
+            return
+        status = self.pozyx.getErrorCode(error_code, self.remote_id)
+        if status == POZYX_SUCCESS:
+            print("ERROR %s on ID %s, %s" %
+                  (operation, "0x%0.4x" % network_id, self.pozyx.getErrorMessage(error_code)))
+            if self.osc_udp_client is not None:
+                self.osc_udp_client.send_message(
+                    "/error", [operation, network_id, error_code[0]])
+        else:
+            self.pozyx.getErrorCode(error_code)
+            print("ERROR %s, couldn't retrieve remote error code, LOCAL ERROR %s" %
+                  (operation, self.pozyx.getErrorMessage(error_code)))
+            if self.osc_udp_client is not None:
+                self.osc_udp_client.send_message("/error", [operation, 0, -1])
+            # should only happen when not being able to communicate with a remote Pozyx.
+
+    def setAnchorsManual(self, save_to_flash=False):
+        """Adds the manually measured anchors to the Pozyx's device list one for one."""
+        status = self.pozyx.clearDevices(remote_id=self.remote_id)
+        for anchor in self.anchors:
+            status &= self.pozyx.addDevice(anchor, remote_id=self.remote_id)
+        if len(self.anchors) > 4:
+            status &= self.pozyx.setSelectionOfAnchors(PozyxConstants.ANCHOR_SELECT_AUTO, len(self.anchors),
+                                                       remote_id=self.remote_id)
+
+        if save_to_flash:
+            self.pozyx.saveAnchorIds(remote_id=self.remote_id)
+            self.pozyx.saveRegisters([PozyxRegisters.POSITIONING_NUMBER_OF_ANCHORS], remote_id=self.remote_id)
+        return status
+
+    def printPublishConfigurationResult(self):
+        """Prints and potentially publishes the anchor configuration result in a human-readable way."""
+        list_size = SingleRegister()
+
+        self.pozyx.getDeviceListSize(list_size, self.remote_id)
+        print("List size: {0}".format(list_size[0]))
+        if list_size[0] != len(self.anchors):
+            self.printPublishErrorCode("configuration")
+            return
+        device_list = DeviceList(list_size=list_size[0])
+        self.pozyx.getDeviceIds(device_list, self.remote_id)
+        print("Calibration result:")
+        print("Anchors found: {0}".format(list_size[0]))
+        print("Anchor IDs: ", device_list)
+
+        for i in range(list_size[0]):
+            anchor_coordinates = Coordinates()
+            self.pozyx.getDeviceCoordinates(device_list[i], anchor_coordinates, self.remote_id)
+            print("ANCHOR, 0x%0.4x, %s" % (device_list[i], str(anchor_coordinates)))
+            if self.osc_udp_client is not None:
+                self.osc_udp_client.send_message(
+                    "/anchor", [device_list[i], int(anchor_coordinates.x), int(anchor_coordinates.y), int(anchor_coordinates.z)])
+
+                # pub.publish(uwb_msg)
+                sleep(0.025)
+        # return(uwb_msg)
+
+    def printPublishAnchorConfiguration(self):
+        """Prints and potentially publishes the anchor configuration"""
+        for anchor in self.anchors:
+            print("ANCHOR,0x%0.4x,%s" % (anchor.network_id, str(anchor.coordinates)))
+            if self.osc_udp_client is not None:
+                self.osc_udp_client.send_message(
+                    "/anchor", [anchor.network_id, int(anchor.coordinates.x), int(anchor.coordinates.y), int(anchor.coordinates.z)])
+                sleep(0.025)
+
+
+if __name__ == "__main__":
+    rospy.init_node("uwb_filter")
+    pub = rospy.Publisher("uwb", UwbMsg)
+    # Check for the latest PyPozyx version. Skip if this takes too long or is not needed by setting to False.
+    check_pypozyx_version = True
+    if check_pypozyx_version:
+        perform_latest_version_check()
+
+    # shortcut to not have to find out the port yourself
+    serial_port = get_first_pozyx_serial_port()
+    if serial_port is None:
+        print("No Pozyx connected. Check your USB cable or your driver!")
+        quit()
+
+    remote_id = 0x6e66                 # remote device network ID
+    remote = False                   # whether to use a remote device
+    if not remote:
+        remote_id = None
+
+    # enable to send position data through OSC
+    use_processing = True
+
+    # configure if you want to route OSC to outside your localhost. Networking knowledge is required.
+    ip = "127.0.0.1"
+    network_port = 8888
+
+    osc_udp_client = None
+    if use_processing:
+        osc_udp_client = SimpleUDPClient(ip, network_port)
+
+    # necessary data for calibration, change the IDs and coordinates yourself according to your measurement
+    anchors = [DeviceCoordinates(0x116F, 1, Coordinates(-1500, 600, 1250)),
+               DeviceCoordinates(0x1166, 1, Coordinates(1500, 0, 1250)),
+               DeviceCoordinates(0x113B, 1, Coordinates(900, 1200, 1250)),
+               DeviceCoordinates(0x1179, 1, Coordinates(300, -900, 1250)),
+               DeviceCoordinates(0x1102, 1, Coordinates(-1500, 600, 1250))]
+
+    # positioning algorithm to use, other is PozyxConstants.POSITIONING_ALGORITHM_TRACKING
+    algorithm = PozyxConstants.POSITIONING_ALGORITHM_UWB_ONLY
+    # positioning dimension. Others are PozyxConstants.DIMENSION_2D, PozyxConstants.DIMENSION_2_5D
+    dimension = PozyxConstants.DIMENSION_3D
+    # height of device, required in 2.5D positioning
+    height = 1000
+
+    pozyx = PozyxSerial(serial_port)
+    r = ReadyToLocalize(pozyx, osc_udp_client, anchors, algorithm, dimension, height, remote_id)
+    r.setup()
+    while not rospy.is_shutdown():
+        uwb_Msg = r.loop()
+        pub.publish(r.uwb_msg)
